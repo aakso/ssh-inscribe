ed754c4060d34e8a91bf59c1063f121002a751d9
Ville Skyttä <ville.skytta@upcloud.com>
2023-06-15 21:29:14 +0300

    feat: server arg handling improvements
    Error out on extra arguments, don't offer completions for them.

--- cmd/server.go
+++ cmd/server.go
@@ -7,9 +7,11 @@ import (
 
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
-	Use:   "server",
-	Short: "Start ssh-inscribe server",
-	Long:  `Start the service`,
+	Use:               "server",
+	Short:             "Start ssh-inscribe server",
+	Long:              `Start the service`,
+	Args:              cobra.NoArgs,
+	ValidArgsFunction: cobra.NoFileCompletions,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if srv, err := server.Build(); err != nil {
 			return err
