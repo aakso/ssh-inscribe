bb3f992a49116266ba333acadc06215f8cc6e427
Ville Skyttä <ville.skytta@upcloud.com>
2023-06-15 22:12:44 +0300

    feat: systemctl daemon-reload, restart service on .deb and .rpm postinst

--- etc/server-post-install.sh
+++ etc/server-post-install.sh
@@ -3,3 +3,12 @@ set -eu
 
 test -f /etc/ssh-inscribe/server_config.yaml ||
     ssh-inscribe defaults >/etc/ssh-inscribe/server_config.yaml
+
+if [ -d /run/systemd/system ]; then
+    systemctl --system daemon-reload || :
+    if test "${1-}" -gt 1 2>/dev/null ||
+       { test "${1-}" = "configure" && test -n "${2-}"; }
+    then
+	systemctl try-restart ssh-inscribe.service
+    fi
+fi
