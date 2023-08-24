006260b28b3ea2c640fd249759782537c64b9c56
Anton Aksola <anton.aksola@upcloud.com>
2023-08-24 08:12:49 +0800

    chore: use a global constant to mark a default RSA algorithm

--- pkg/util/sshkey.go
+++ pkg/util/sshkey.go
@@ -4,12 +4,15 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+// DefaultRSAKeyAlgorithm is the default algorithm with RSA keys.
+const DefaultRSAKeyAlgorithm = ssh.KeyAlgoRSASHA256
+
 func SignatureFormatFromSigningOptionAndCA(opt string, ca ssh.PublicKey) string {
 	switch {
 	case ca != nil && ca.Type() == ssh.KeyAlgoED25519:
 		return ssh.KeyAlgoED25519
 	case ca != nil && ca.Type() == ssh.KeyAlgoRSA && opt == "":
-		return ssh.KeyAlgoRSASHA256
+		return DefaultRSAKeyAlgorithm
 	default:
 		return opt
 	}
